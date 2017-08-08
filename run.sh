8b72e8896986852a74d9a844cdb868c5f4c5cc55
Beny H <Beny.Haddad@ericom.com>
2017-08-08 14:08:54 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+############################################
+#####   Ericom Shield Run              #####
+#######################################BH###
+
+ES_PATH=/usr/local/ericomshield
+
+#Check if we are root
+if (( $EUID != 0 )); then
+#    sudo su
+        echo " Please run it as Root"
+        echo "sudo" $0 $1 $2
+        exit
+fi
+cd $ES_PATH
+
+echo "Running deploy-shield.sh:"
+./deploy-shield.sh
+
