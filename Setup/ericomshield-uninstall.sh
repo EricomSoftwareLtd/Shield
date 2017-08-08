dbcf7b47fc5ce1497dabaf7dbdee5c5d6b02ae2d
Beny H <Beny.Haddad@ericom.com>
2017-08-08 16:17:10 +0300

    Create ericomshield-uninstall.sh

---
+++ Setup/ericomshield-uninstall.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+############################################
+#####   Ericom Shield Installer        #####
+#######################################BH###
+
+#Check if we are root
+if (( $EUID != 0 )); then
+#    sudo su
+        echo "Usage:" $0
+        echo " Please run it as Root"
+        echo "sudo" $0
+        exit
+fi
+ES_PATH="/usr/local/ericomshield"
+LOGFILE="$ES_PATH/ericomshield.log"
+STACK_NAME=shield
+
+cd $ES_PATH
+
+service ericomshield stop
+docker-compose down
+
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
+echo "Uninstalling Ericom Shield"
+echo "$(date): Uninstalling Ericom Shield" >> "$LOGFILE"
+mv "$LOGFILE" ..
+systemctl --global disable ericomshield-updater.service
+systemctl --global disable ericomshield.service
+systemctl daemon-reload
+
+rm /etc/init.d/ericomshield
+
+echo "***********       Removing EricomShield "
+echo "***********       "
+docker stack rm $STACK_NAME
+docker swarm leave -f
+
+docker system prune -f -a
+
+rm -f $ES_PATH/*
+rm -f $ES_PATH/.*
+
+
+echo "Done!"
