fd9d77c398f067463d3a22375a1c0a7b872159fb
Beny H <Beny.Haddad@ericom.com>
2017-08-10 14:16:15 +0300

    Update status.sh

--- Setup/status.sh
+++ Setup/status.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-############################################
-#####   Ericom Shield Status              #####
-#######################################BH###
-ES_PATH="/usr/local/ericomshield"
-ES_RUN="$ES_PATH/run.sh"
-ES_SWARM_FILE="$ES_PATH/.esswarm"
-
 #Check if we are root
 if (( $EUID != 0 )); then
 #    sudo su
@@ -17,13 +9,21 @@ cd $ES_PATH
 
 NUM_EXPECTED_SERVICES=$(grep -c image docker-compose.yml)
 NUM_RUNNING_SERVICES=$(docker service ls |wc -l)
+NUM_EXPECTED_REP=$(docker service ls | grep -c "/[1-2] ")
+NUM_RUNNING_REP=$(docker service ls | grep -c "[1-2]/")
 
 if [ "$1" == "-a" ]; then
   docker service ls
 fi
 
 if [ $NUM_RUNNING_SERVICES -ge  $NUM_EXPECTED_SERVICES ]; then
-   echo "***************     Ericom Shield is running"
+   a=
+   if [ $NUM_RUNNING_REP -ge  $NUM_EXPECTED_REP ]; then
+     echo "***************     Ericom Shield is running"
+    else
+     echo "***************     Ericom Shield is loading  ($NUM_RUNNING_REP/$NUM_EXPECTED_REP)"
+     exit 1
+   fi
   else
    echo " Ericom Shield is not running properly on this system"
    exit 1
