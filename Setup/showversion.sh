520e113b804c926d8a67cba7e75b69fa2e004de3
Beny H <Beny.Haddad@ericom.com>
2017-08-10 11:40:11 +0300

    Create showversion.sh

---
+++ Setup/showversion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+############################################
+#####   Ericom Shield ShowVersion      #####
+#######################################BH###
+
+ES_PATH=/usr/local/ericomshield
+ES_VERSION="$ES_PATH/.version"
+
+cat $ES_VERSION
