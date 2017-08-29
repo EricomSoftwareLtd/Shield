db94d4f721ffa9b501c1a2416d00afe88832eac1
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-08-29 13:46:57 +0300

    Add clean script

---
+++ Setup/shield-clean-system.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker system prune -af
+filename=./shield-version-dev.txt
+COUNTER=0
+while read -r line; do
+    if [ "$COUNTER" -ge "2" ]; then
+        arr=($line)
+        docker pull "securebrowsing/${arr[1]}"
+        docker tag "securebrowsing/${arr[1]}" "securebrowsing/${arr[0]}"
+    fi
+    COUNTER=$(($COUNTER+1))
+done < "$filename"
