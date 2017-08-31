5841fbcf6ecb9cc5e2b02eb35b0a8d8d97a86a8e
Beny H <Beny.Haddad@ericom.com>
2017-08-31 11:44:36 +0300

    Adding  --assume-yes -y

--- Setup/ericomshield-setup.sh
+++ Setup/ericomshield-setup.sh
@@ -89,8 +89,8 @@ function install_docker {
          echo "***************     Installing docker-engine"
          apt-get --assume-yes -y install apt-transport-https
          apt-get update
-         apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual  
-         apt-get install apt-transport-https ca-certificates software-properties-common
+         apt-get --assume-yes -y  install linux-image-extra-$(uname -r) linux-image-extra-virtual  
+         apt-get --assume-yes -y  install apt-transport-https ca-certificates software-properties-common
          curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
          add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
          apt-get update         
