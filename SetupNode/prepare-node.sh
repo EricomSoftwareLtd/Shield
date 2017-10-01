974bd31e59b761294c4011159fa58ca6a09cefd7
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-01 15:04:02 +0300

    Fix script to be more clear

--- SetupNode/prepare-node.sh
+++ SetupNode/prepare-node.sh
@@ -2,6 +2,10 @@
 
 MACHINE_USER=$(whoami)
 
+echo "########################## $MACHINE_USER Going to be super user #########################################"
+
 sudo su
+
+echo "################################################ Make sudo without password ###################################"
 
 echo "$MACHINE_USER    ALL=(ALL:ALL)   NOPASSWD: ALL" >> /etc/sudoers
