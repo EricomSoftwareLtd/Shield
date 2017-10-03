f3c202b6f47c41f6bceaa47584a2c9dbd25e5768
Beny H <Beny.Haddad@ericom.com>
2017-10-03 13:18:11 +0300

    Update ericomshield-setup.sh

--- Setup/ericomshield-setup.sh
+++ Setup/ericomshield-setup.sh
@@ -28,7 +28,7 @@ ES_VER_FILE_BAK="$ES_PATH/shield-version.bak"
 ES_uninstall_FILE="$ES_PATH/ericomshield-uninstall.sh"
 EULA_ACCEPTED_FILE="$ES_PATH/.eula_accepted"
 
-ES_SETUP_VER="17.40b-Setup"
+ES_SETUP_VER="17.40c-Setup"
 BRANCH="master"
 
 MIN_FREE_SPACE_GB=5
@@ -172,11 +172,11 @@ function install_docker() {
 	#Docker Installation of a specific Version
         curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
         sudo apt-add-repository "deb https://apt.dockerproject.org/repo debian-$(lsb_release -cs) main"
-        sudo apt-get update
+        sudo apt-get -qq update
 
         sudo apt-cache policy docker-ce
-	echo "Installing Docker: docker-ce=$DOCKER_VERSION~ce-2~ubuntu"
-	sudo apt-get -y --assume-yes --allow-downgrades install docker-ce=$(DOCKER_VERSION)~ce-2~ubuntu
+	echo "Installing Docker: docker-ce=$DOCKER_VERSION~ce-0~ubuntu"
+	sudo apt-get -y --assume-yes --allow-downgrades install docker-ce=$(DOCKER_VERSION)~ce-0~ubuntu
     else
         echo " ******* docker-engine $DOCKER_VERSION is already installed"
     fi
