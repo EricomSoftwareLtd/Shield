10258e872fb2c0a64d4ff090959766007455245a
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-09 10:18:56 +0300

    Temp save changes

--- Setup/deployshield-dev.sh
+++ Setup/deployshield-dev.sh
@@ -191,4 +191,4 @@ create_proxy_env_file
 
 docker node update --label-add browser=yes --label-add shield_core=yes --label-add management=yes $SYS_LOG_HOST
 
-docker stack deploy -c $ES_YML_FILE $STACK_NAME --with-registry-auth
+docker stack deploy -c $ES_YML_FILE $STACK_NAME #--with-registry-auth
