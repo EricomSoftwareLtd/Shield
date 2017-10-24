2b4fa0f9382a2489e496e0f1425fae28bc9f731c
Beny H <Beny.Haddad@ericom.com>
2017-10-24 18:47:19 +0300

    Update ericomshield-setup.sh

--- Setup/ericomshield-setup.sh
+++ Setup/ericomshield-setup.sh
@@ -30,7 +30,7 @@ ES_uninstall_FILE="$ES_PATH/ericomshield-uninstall.sh"
 EULA_ACCEPTED_FILE="$ES_PATH/.eula_accepted"
 
 ES_SETUP_VER="17.43-Setup"
-BRANCH="Install-Staging"
+BRANCH="master"
 
 MIN_FREE_SPACE_GB=5
 DOCKER_USER="ericomshield1"
