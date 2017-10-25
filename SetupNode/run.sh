31f62bfd30d78e5efdb2c4c6bdf799ceb0e8d96b
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-10-25 14:48:10 +0300

    Rewrite ericomshield-setup-node.sh now run with container

--- SetupNode/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-docker run --rm -it \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-	-v $(which docker):/usr/bin/docker \
-	--network host \
-    securebrowsing/node-installer:1.0 ./setup-node.sh "${@}"
