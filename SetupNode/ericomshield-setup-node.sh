ac60356e3ee9d7e7d733e0bf92c684981b0c86e1
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2017-11-23 17:04:55 +0200

    Finish status and cert mode... Remain make hosts as well

--- SetupNode/ericomshield-setup-node.sh
+++ SetupNode/ericomshield-setup-node.sh
@@ -3,10 +3,11 @@
 #####   Ericom Shield Installer        #####
 #######################################LO###
 
-export DOCKER_TAG=171106-09.02
+export DOCKER_TAG=171123-17.00
 
 docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
 	-v $(which docker):/usr/bin/docker \
+	-v $(pwd):/certificate \
 	--network host \
     securebrowsing/node-installer:$DOCKER_TAG ./setup-node.sh "${@}"
