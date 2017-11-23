4e82b0dfc6519e05683b7c7a61a7d0284c852f7a
Beny H <Beny.Haddad@ericom.com>
2017-11-23 11:47:01 +0200

    Update ericomshield-setup.sh

--- Setup/ericomshield-setup.sh
+++ Setup/ericomshield-setup.sh
@@ -30,7 +30,7 @@ ES_uninstall_FILE="$ES_PATH/ericomshield-uninstall.sh"
 EULA_ACCEPTED_FILE="$ES_PATH/.eula_accepted"
 ES_MY_IP_FILE="$ES_PATH/.es_ip_address"
 
-ES_SETUP_VER="17.45-Setup"
+ES_SETUP_VER="17.47-Setup"
 if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
