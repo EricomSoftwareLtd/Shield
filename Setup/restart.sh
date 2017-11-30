79972a1913b3e0183a4f9d311e110c15d8877a5b
Beny H <Beny.Haddad@ericom.com>
2017-11-30 23:25:32 +0200

    Update restart.sh

--- Setup/restart.sh
+++ Setup/restart.sh
@@ -3,6 +3,14 @@
 #####   Ericom Shield Restart          #####
 #######################################BH###
 
+#Check if we are root
+if ((EUID != 0)); then
+    #    sudo su
+    echo " Please run it as Root"
+    echo "sudo $0 $@"
+    exit
+fi
+
 ES_PATH=/usr/local/ericomshield
 echo "***********       Restarting EricomShield "
 echo "***********       "
