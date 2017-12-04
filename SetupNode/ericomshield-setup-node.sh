e32318a427cf12dff047ea75432b9a514933433c
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2017-12-04 11:36:37 +0200

    Change container version

--- SetupNode/ericomshield-setup-node.sh
+++ SetupNode/ericomshield-setup-node.sh
@@ -3,7 +3,7 @@
 #####   Ericom Shield Installer        #####
 #######################################LO###
 
-export DOCKER_TAG=latest
+export DOCKER_TAG=171204-11.35
 
 docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
