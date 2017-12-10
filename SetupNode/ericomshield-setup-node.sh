34bfc69a16f6291f19988831b8b617549fcb6892
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2017-12-10 15:10:59 +0200

    Update container label

--- SetupNode/ericomshield-setup-node.sh
+++ SetupNode/ericomshield-setup-node.sh
@@ -3,7 +3,7 @@
 #####   Ericom Shield Installer        #####
 #######################################LO###
 
-export DOCKER_TAG=171210-14.23
+export DOCKER_TAG=171210-15.08
 
 docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
