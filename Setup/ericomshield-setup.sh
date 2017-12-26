151c73e616bc1f51e7d56a46cac038e94186fb92
Beny H <Beny.Haddad@ericom.com>
2017-12-26 17:15:29 +0200

    Update ericomshield-setup.sh

--- Setup/ericomshield-setup.sh
+++ Setup/ericomshield-setup.sh
@@ -602,8 +602,9 @@ fi
 if [ "$UPDATE" == false ]; then
     # New Installation
     if [ "$ES_CONFIG_STORAGE" = "yes" ]; then
-       if [ "$ES_DEV" == true ] || [ "$ES_STAGING" == true ] #Do not set overlay2 for production until 18.01
-        set_storage_driver
+       if [ "$ES_DEV" == true ] || [ "$ES_STAGING" == true ]; then #Do not set overlay2 for production until 18.01
+          set_storage_driver
+	fi
     fi
     
     create_shield_service
