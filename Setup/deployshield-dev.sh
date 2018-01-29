24dc3bebc4b73d524bc8706abbd27db309e37d39
Andrew Novikov <Andrew.Novikov@artezio.com>
2018-01-29 13:09:03 +0300

    Pull securebrowsing/shield_swarm-exec: beforehand

--- Setup/deployshield-dev.sh
+++ Setup/deployshield-dev.sh
@@ -177,12 +177,13 @@ if [ "$AM_I_LEADER" == true ]; then
     echo "Copy docker-compose.yml across all manager nodes"
     docker service rm copy_yaml 2>/dev/null || true
     docker config rm yaml 2>/dev/null || true
+    docker pull "securebrowsing/shield_swarm-exec:$DOCKER_SWARMEXEC_TAG"
     docker config create yaml "/usr/local/ericomshield/$ES_YML_FILE"
     docker service create --constraint "node.labels.management==yes" --name copy_yaml --mode=global --restart-condition none \
         --mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" \
         --mount "type=bind,source=/usr/local/ericomshield,target=/mnt" \
         --config src=yaml,target="/tmp/$ES_YML_FILE" \
-        securebrowsing/shield_swarm-exec:$DOCKER_SWARMEXEC_TAG \
+        "securebrowsing/shield_swarm-exec:$DOCKER_SWARMEXEC_TAG" \
         /bin/sh -c "cp -f /tmp/$ES_YML_FILE /mnt/$ES_YML_FILE && sleep 50"
     docker service rm copy_yaml
     docker config rm yaml
