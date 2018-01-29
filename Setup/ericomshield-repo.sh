ce90b489ffdbf5a72f34f5d4225f236a0b0dc211
Beny H <Beny.Haddad@ericom.com>
2018-01-29 13:08:45 +0200

    Update ericomshield-repo.sh

--- Setup/ericomshield-repo.sh
+++ Setup/ericomshield-repo.sh
@@ -7,7 +7,6 @@ if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
 #BRANCH="Install-Staging"
-
 # Development Repository: (Latest)
 ES_repo_setup="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/ericomshield-setup.sh"
 ES_repo_run="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/run.sh"
@@ -25,6 +24,25 @@ ES_repo_setup_node="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$
 ES_repo_shield_nodes="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/shield-nodes.sh"
 ES_repo_pre_check="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/master/Setup/shield_pre_install_check.sh"
 ES_repo_shield_aliases="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/master/Setup/.shield_aliases"
+ES_repo_restore="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/restore.sh"
+
+ES_cmd_setup="setup.sh"
+ES_cmd_run="start.sh"
+ES_cmd_update="update.sh"
+ES_cmd_version="version.sh"
+ES_cmd_stop="stop.sh"
+ES_cmd_status="status.sh"
+ES_cmd_restart="restart.sh"
+ES_cmd_uninstall="uninstall.sh"
+ES_cmd_ip="~/show-my-ip.sh"
+ES_cmd_systemd_updater_service="ericomshield-updater.service"
+ES_cmd_sysctl_shield_conf="sysctl_shield.conf"
+ES_cmd_EULA="Ericom-EULA.txt"
+ES_cmd_setup_node="setup-node.sh"
+ES_cmd_shield_nodes="nodes.sh"
+ES_cmd_pre_check="shield_pre_install_check.sh"
+ES_cmd_shield_aliases=".shield_aliases"
+ES_cmd_restore="restore.sh"
 
 # Production Version Repository: (Release)
 ES_repo_ver="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/shield-version.txt"
@@ -37,6 +55,5 @@ ES_repo_staging_yml="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/
 ES_repo_dev_ver="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/shield-version-dev.txt"
 ES_repo_dev_yml="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/docker-compose_dev.yml"
 ES_repo_swarm_dev_sh="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/deployshield-dev.sh"
-ES_repo_restore_dev_sh="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/restore.sh"
 
 ES_repo_pocket_yml="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/docker-compose_pocket.yml"
