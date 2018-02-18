670e06a2366a19bdde4a50666a399f68fbfb0c87
Beny H <Beny.Haddad@ericom.com>
2018-02-18 17:33:43 +0200

    Update showversion.sh

--- Setup/showversion.sh
+++ Setup/showversion.sh
@@ -4,10 +4,21 @@
 #######################################BH###
 
 ES_PATH=/usr/local/ericomshield
-ES_VERSION="$ES_PATH/shield-version.txt"
+ES_VER_FILE="$ES_PATH/shield-version.txt"
+
+function showversion() {
+    echo "Ericom Shield Version:"
+    while read -r ver; do
+        if [ "${ver:0:1}" == '#' ]; then
+            echo "$ver"
+        else
+            echo "$ver" | awk '{print $2}'
+        fi
+    done <"$ES_VER_FILE"
+}
 
 docker version
 
 echo "********************************************************"
 
-cat $ES_VERSION
+showversion
