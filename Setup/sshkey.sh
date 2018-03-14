a2b4d382d579809a6190bac2a7074e35bb517ecb
ozlevka-ericom <lev.ozeryansky@ericom.com>
2018-03-14 11:28:52 +0200

    Add script to generate key

---
+++ Setup/sshkey.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+    docker run --rm -it \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v $(which docker):/usr/bin/docker \
+        -v /usr/local/ericomshield:/usr/local/ericomshield \
+        securebrowsing/shield-autoupdate:test sshkey
