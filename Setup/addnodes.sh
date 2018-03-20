2050fc31320e906b2ec96cdaca7f83dc2395b0a1
Beny H <Beny.Haddad@ericom.com>
2018-03-20 16:25:18 +0200

    Create  addnodes.sh

---
+++ Setup/addnodes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+############################################
+#####   Ericom Shield AddNode          #####
+#######################################LO###
+
+ES_PATH=/usr/local/ericomshield
+
+#Check if we are root
+if ((EUID != 0)); then
+    #    sudo su
+    echo " Please run it as Root"
+    echo "sudo $0 $@"
+    exit
+fi
+cd $ES_PATH
+
+echo "Running  addnodes.sh:"
+
+
+if [ ! -f "$ES_VER_FILE" ]; then
+   echo "$(date): Ericom Shield Update: Cannot find version file" >>"$LOGFILE"   
+   exit 1
+fi
+CONTAINER_TAG="$(grep -r 'node-installer' $ES_VER_FILE | cut -d' ' -f2)"
+
+docker run --rm -it \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+	-v $(which docker):/usr/bin/docker \
+	-v /usr/local/ericomshield:/install \
+	-v $(pwd):/certificate \
+	--network host \
+    securebrowsing/node-installer:$CONTAINER_TAG ./setup-node.sh "${@}"
