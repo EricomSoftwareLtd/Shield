9a292155c43f1890867bbf08a22e52643c875a26
ozlevka-ericom <lev.ozeryansky@ericom.com>
2018-05-06 15:37:43 +0300

    Make shell wrapper for system container

--- Setup/status.sh
+++ Setup/status.sh
@@ -13,6 +13,7 @@ fi
 
 ES_PATH=/usr/local/ericomshield
 cd $ES_PATH
+ES_VER_FILE="$ES_PATH/shield-version.txt"
 
 NUM_EXPECTED_SERVICES=$(grep -c image docker-compose.yml)
 NUM_RUNNING_SERVICES=$(docker service ls | wc -l)
@@ -24,51 +25,17 @@ if [ $BROWSER_RUNNING -eq 0 ]; then
     NUM_RUNNING_REP=$((NUM_RUNNING_REP + 1))
 fi
 
-
-while [ $# -ne 0 ]; do
-    arg="$1"
-    case "$arg" in
-    -a | --all)
-        docker service ls
-        echo "------------------------------------------------------------------------------"
-        echo
-        var=$(curl --silent -q --proxy http://127.0.0.1:3128 http://shield-stats 2>&1)
-        echo ${var:16:330}
-        echo
-        echo "------------------------------------------------------------------------------"
-        echo
-        ;;
-    -s | --services)
-          ./addnodes.sh --status
-        ;;
-    -n | --nodes)
-          ./addnodes.sh --node-status
-        ;;
-    -h | --help)
-        echo "Usage: $0 [-a | --all] [-s | --services] [-n | --nodes] [-h | --help]"
-        echo "           -a --all - lists all services in the system"
-        echo "           -s --services - prints a detailed report of the services in the system and which service runs on which node"
-        echo "           -n --nodes - lists the nodes in the multi-machine system, including data about each node"
-        exit
-        ;;
-    *)
-        echo "Usage: $0 [-a | --all] [-s | --services] [-n | --nodes] [-h | --help]"
-        exit
-        ;;
-    esac
-    shift
-done
-
-if [ $NUM_RUNNING_SERVICES -ge $NUM_EXPECTED_SERVICES ]; then
-    if [ $NUM_RUNNING_REP -ge $NUM_EXPECTED_REP ]; then
-        echo "***************     Ericom Shield is running"
-    else
-        echo "***************     Ericom Shield is loading  ($NUM_RUNNING_REP/$NUM_EXPECTED_REP)"
-        exit 1
-    fi
-else
-    echo " Ericom Shield is not running properly on this system"
-    exit 1
+CONTAINER_TAG="$(grep -r 'shield-autoupdate' $ES_VER_FILE | cut -d' ' -f2)"
+if [ "$CONTAINER_TAG" = "" ]; then
+   CONTAINER_TAG="shield-autoupdate:180328-06.56-1731"
 fi
+
+CONTAINER_TAG="shield-autoupdate:latest"
+
+docker run --rm  -it \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -v $(which docker):/usr/bin/docker \
+       -v "$ES_PATH:$ES_PATH" \
+       "securebrowsing/$CONTAINER_TAG" status "${@}"
 
 exit 0
