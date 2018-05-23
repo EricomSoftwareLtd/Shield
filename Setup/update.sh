fc1caa33dea28387cd7559975382dafb950d7277
Beny H <Beny.Haddad@ericom.com>
2018-05-23 16:40:04 +0300

    Update update.sh

--- Setup/update.sh
+++ Setup/update.sh
@@ -22,6 +22,7 @@ ES_CHANNEL=""
 UPDATE_LOG_FILE="$ES_PATH/lastoperation.log"
 ES_CONFIG_FILE="$ES_PATH/docker-compose.yml"
 VERSION_REGEX="SHIELD_VER=([a-zA-Z0-9_:]+)"
+ES_BRANCH_FILE="$ES_PATH/.esbranch"
 
 cd "$ES_PATH" || exit
 
@@ -50,7 +51,6 @@ function set_channel_mode() {
    fi
 }
 
-
 # if command is update (from cli or based on the previous ifs, then check the channel based on the file) - should be handled in the container
 if [ "$ARGS" = "update" ]; then
     set_channel_mode
@@ -66,6 +66,7 @@ while [ $# -ne 0 ]; do
     case "$arg" in
     -v | --version)
         BRANCH=$2
+        echo $BRANCH > "$ES_BRANCH_FILE"
         if [ -z "$ES_CHANNEL" ]; then
             set_channel_mode
         fi
@@ -76,7 +77,6 @@ while [ $# -ne 0 ]; do
     --staging)
         ES_CHANNEL="--staging"
         ;;
-
     --verbose)
         FULL_OUTPUT="--verbose"
         ;;
@@ -89,7 +89,6 @@ while [ $# -ne 0 ]; do
     esac
     shift
 done
-
 
 function get_latest_version() {
     cd "$ES_PATH"
@@ -117,13 +116,20 @@ function get_latest_version() {
     fi
 }
 
-
 function read_current_version() {
     ver=$(cat "$ES_CONFIG_FILE" | grep "SHIELD_VER=")
     if [[ $ver =~ $VERSION_REGEX ]]; then
         CURRENT_SHIELD_VERSION="${BASH_REMATCH[1]}"
     fi
 }
+
+if [ -z "$BRANCH" ]; then
+    if [ ! -f "$ES_BRANCH_FILE" ]; then
+      BRANCH=cat "$ES_BRANCH_FILE"
+     else
+      BRANCH="master"
+    fi  
+fi
 
 get_latest_version
 if [ -z "$FORCE_RUN" ]; then
@@ -140,6 +146,7 @@ fi
 CONTAINER_TAG="$(grep -r 'shield-autoupdate' $ES_VER_FILE | cut -d' ' -f2)"
 if [ "$CONTAINER_TAG" = "" ]; then
    CONTAINER_TAG="shield-autoupdate:180328-06.56-1731"
+   echo "Warning: shield-autoupdate not found in $ES_VER_FILE, using default tag"
 fi
 
 function wait_upgrade_process_finish() {
@@ -155,7 +162,6 @@ function wait_upgrade_process_finish() {
 
     echo "Done!"
 }
-
 
 function upgrade_docker_version() {
     NEXT_VERSION=$(cat "$ES_VER_FILE" | grep 'docker-version' | awk '{ print $2 }')
@@ -180,7 +186,6 @@ echo "$(date): Ericom Shield Update: Running Update" >>"$LOGFILE"
 if [ -f "$UPDATE_LOG_FILE" ]; then
     rm -f $UPDATE_LOG_FILE
 fi
-
 
 if [ -z "$AUTOUPDATE"  ]; then
     DOCKER_RUN_PARAM="-it"
