17fec06bfbce0636699a213d845f14dc83b33bad
Andrew Novikov <as.asaw@gmail.com>
2018-06-25 15:44:48 +0300

    Fix https://github.com/EricomSoftwareLtd/SB/issues/3377#issuecomment-399752991

--- Utils/download-docker.sh
+++ Utils/download-docker.sh
@@ -3,14 +3,12 @@
 #####   Ericom Shield Docker Installer #####
 ####################################AN#BH###
 
-#Check if we are root
-if ((EUID != 0)); then
-    #    sudo su
-    echo "Usage: $0"
-    echo " Please run it as Root"
-    echo "sudo $0 $@"
-    exit
-fi
+
+sudo apt-get --assume-yes -y install apt-transport-https software-properties-common
+
+# Add Docker repo
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Create docker_debs empty dir if necessary
 if [ ! -d ./docker_debs ]; then
@@ -18,14 +16,14 @@ if [ ! -d ./docker_debs ]; then
     chmod 0755 ./docker_debs
 fi
 
-PACKAGES="docker-ce=18.03.0~ce* uuid-runtime curl"
+PACKAGES="docker-ce=18.03.0~ce* uuid-runtime curl \
+        libgnutls-openssl27 libpam-systemd systemd udev"
 
 sudo apt update
 
 cd  ./docker_debs && \
     rm -f *.deb && \
-    apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends ${PACKAGES} | grep "^\w") && \
-    rm -f *i386.deb && \
+    apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances ${PACKAGES} | grep "^\w" | grep -v ":i386$") && \
     cd ..
 
 tar cvfz docker-debs.tgz ./docker_debs
