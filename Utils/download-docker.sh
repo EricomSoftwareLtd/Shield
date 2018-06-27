f0518a8e18a0f87bd0a05d60d93f091c773b3c67
Andrew Novikov <as.asaw@gmail.com>
2018-06-27 14:43:53 +0300

    Specify Docker version separately; download the specified version instead of the latest one

--- Utils/download-docker.sh
+++ Utils/download-docker.sh
@@ -3,6 +3,7 @@
 #####   Ericom Shield Docker Installer #####
 ####################################AN#BH###
 
+DOCKER_VERSION="18.03.0~ce*"
 
 sudo apt-get --assume-yes -y install apt-transport-https software-properties-common
 
@@ -16,14 +17,14 @@ if [ ! -d ./docker_debs ]; then
     chmod 0755 ./docker_debs
 fi
 
-PACKAGES="docker-ce=18.03.0~ce* uuid-runtime curl \
+PACKAGES="docker-ce=${DOCKER_VERSION} uuid-runtime curl \
         libgnutls-openssl27 libpam-systemd systemd udev"
 
 sudo apt update
 
 cd  ./docker_debs && \
     rm -f *.deb && \
-    apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances ${PACKAGES} | grep "^\w" | grep -v ":i386$") && \
+    apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances ${PACKAGES} | grep "^\w" | grep -v ":i386$" | sed "s#docker-ce#docker-ce=${DOCKER_VERSION}#") && \
     cd ..
 
 tar cvfz docker-debs.tgz ./docker_debs
