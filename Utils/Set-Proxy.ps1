e447a1d4afcc777e11cad860a425e8c826636656
Beny H <Beny.Haddad@ericom.com>
2018-07-02 14:57:31 +0300

    Update Set-Proxy.ps1

--- Utils/Set-Proxy.ps1
+++ Utils/Set-Proxy.ps1
@@ -1,71 +1,65 @@
+#################################################################################
+#####                      Ericom Shield Set Proxy Script                   #####
+### Need to edit file and set correct values for shield server and pac file #####
+###	Need to run the file on the end-users’ laptops or add it to the GPO     #####
+#########################################################################BH######
 
 function Configure-Proxy ($Proxy, $Port)
 {
-# Function that actually does the configuring of the proxy settings.
-Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyEnable -Value 1
-Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyServer -Value $Proxy":"$Port
-Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyOverride -Value "&lt;local&gt;"
+   # Function that actually does the configuring of the proxy settings.
+   Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyEnable -Value 1
+   if( $Proxy )
+   {
+    Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyServer -Value $Proxy":"$Port
+   }
+   if( $PAC_file )
+   {
+    Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name AutoConfigURL -Value $PAC_file
+   } 
+   Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyOverride -Value "<local>;"
 }
 
-while(true){
-  start-sleep -s 60
-  Set-Proxy
-}
-
-Function Set-Proxy
+function Set-Proxy
 {
 #  $NetworkConfig = @(ipconfig.exe /all)
   $a = Get-NetAdapter | Where-Object {$_.status -eq "Up"}
   $b = $a.ifIndex
   $NetworkConfig = Get-DnsClient -InterfaceIndex $b
 
-  $Proxy = "shield-jer"
+  $Proxy = "" # Set the Proxy Name or IP (e.g. "shield-jer")
   $Port = "3128"
-  $Network = "google.local"
-
+  $Network = "ericom.local"
+  $PAC_file = "" # Set the URL to the PAC File (e.g. "http://shield-jer/shield.pac")
  
-  # If the network name found by the NIC matches to one in the CSV the proxy
-  # settings are configured by parsing the right values to the Configure-Proxy function.
-#  if ( $NetworkConfig -like "$Network" )
-  if ($NetworkConfig.suffix -eq "$Network")
-    {
-#    $msgBoxInput =  [System.Windows.MessageBox]::Show('You are at the office in Network. Would you like to set the Proxy?','Setting Proxy','YesNo','Info')
-#    switch  ($msgBoxInput) {
-#    'Yes' {
-          Configure-Proxy $Proxy $Port
-#          }
-#    'No' {
-    ## Do something
-#        }
-#     }	  
-    }
+# settings are configured by parsing the right values to the Configure-Proxy function.
+  if ( $NetworkConfig.suffix -eq "$Network" )
+  {
+     $ProxyEnabled = Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyEnable
+     if ( $ProxyEnabled.ProxyEnable -eq 0 )
+     {   
+       # If used as a service, need to be disabled    
+       $msgBoxInput =  [System.Windows.MessageBox]::Show('You are at the office in Network. Would you like to set the Proxy?','Setting Proxy','YesNo','Info')
+       switch  ($msgBoxInput) {
+        'Yes' {
+              Configure-Proxy $Proxy $Port
+         }
+        'No' {
+         ## Do something
+         }
+     } 
+     }	  
+  }
     # If no value applies, disable the proxy configuration.
-   else
-    {
+  else
+  {
       Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyEnable -Value 0
-    }
+      $wshell = New-Object -ComObject Wscript.Shell
+      $wshell.Popup("Proxy Disabled",0,"Done",0x1)
+  }
 }
  
-#Function Set-Proxy-Csv
-#{
-#  $NetworkConfig = @(ipconfig.exe /all)
-#  $Match = $false
-#  ForEach ($customer in $customers)
-#  {
-#    $Port = $Customer.Port
-##    $Proxy = $Customer.Proxy
-#    $Network = $Customer.Network
-#    # If the network name found by the NIC matches to one in the CSV the proxy
-#    # settings are configured by parsing the right values to the Configure-Proxy function.
-#    if ($NetworkConfig -like "$Network")
-#    {
-#      Configure-Proxy $Proxy $Port
-#      $Match = $true
-#    }
-#    # If no value applies, disable the proxy configuration.
-#    elseif (!$match)
-#    {
-#      Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 0
-#    }
-#  }
-#}
+#    If used as a service, need to be disabled
+while( $true ){
+  Set-Proxy
+  start-sleep -s 60  
+}
