f851a0f4aef4dd56db5622c0d9c56111a0859cf7
Beny H <Beny.Haddad@ericom.com>
2018-08-22 16:09:03 +0300

    Update setup.sh

--- Setup/setup.sh
+++ Setup/setup.sh
@@ -6,13 +6,26 @@
 #Check if we are root
 if ((EUID != 0)); then
     #    sudo su
-    echo "Usage: $0 [-force] [-autoupdate] [-dev] [-staging] [-quickeval] [-usage]"
+    echo "Usage: $0 [-force] [-autoupdate] [-staging] [-quickeval] [-usage]"
     echo " Please run it as Root"
     echo "sudo $0 $@"
     exit
 fi
 
+ES_PATH="/usr/local/ericomshield"
+if [ -d  "$ES_PATH" ]; then
+   cd "$ES_PATH"
+fi
+
+if [ -z "$BRANCH" ]; then
+    if [ -f "$ES_BRANCH_FILE" ]; then
+        BRANCH=$(cat "$ES_BRANCH_FILE")
+    else
+        BRANCH="master"
+    fi
+fi
+
 rm ./ericomshield-setup.sh
-wget https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/master/Setup/ericomshield-setup.sh
+wget https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/ericomshield-setup.sh
 chmod +x ericomshield-setup.sh
 bash ./ericomshield-setup.sh $@
