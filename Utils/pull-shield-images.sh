913fa786a6e91831c2cd911f0d2ef55be373b2a8
BenyH <Beny.Haddad@ericom.com>
2018-09-06 14:16:42 +0300

    docker login for pull_images

--- Utils/pull-shield-images.sh
+++ Utils/pull-shield-images.sh
@@ -2,7 +2,6 @@
 ############################################
 #####   Ericom Shield Pull Images      #####
 #######################################BH###
-ES_SETUP_VER="Setup:18.08-3007"
 
 #Check if we are root
 if ((EUID != 0)); then
@@ -16,8 +15,23 @@ ES_VER_FILE_NEW="$ES_PATH/shield-version-new.txt"
 DEV_BRANCH="Dev"
 STAGING_BRANCH="Staging"
 BRANCH="master"
-
+DOCKER_USER="ericomshield1"
+DOCKER_SECRET="Ericom98765$"
 ES_repo_ver="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Setup/shield-version.txt"
+
+function docker_login() {
+    if [ "$(docker info | grep -c Username)" -eq 0 ]; then
+       echo "docker login" $DOCKER_USER $DOCKER_SECRET
+       echo "$DOCKER_SECRET" | docker login --username=$DOCKER_USER --password-stdin
+    fi
+
+    if [ $? == 0 ]; then
+       echo "Login Succeeded!"
+     else
+       echo "Cannot Login to docker, Exiting!"
+       exit 1
+    fi
+}
 
 function get_shield_version() {
     echo "Getting shield install files"
@@ -45,6 +59,7 @@ function pull_images() {
     done <"$ES_VER_FILE_NEW"
 }
 
+    docker_login
     get_shield_version
     echo "************ Pull shield images"
     pull_images
