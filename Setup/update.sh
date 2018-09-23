3bfad09796f0f6f3ce1503849bae809ceea922d8
ozlevka-ericom <lev.ozeryansky@ericom.com>
2018-09-23 12:46:42 +0300

    Fix code for be able call get_latest_version from switches wile

--- Setup/update.sh
+++ Setup/update.sh
@@ -131,6 +131,31 @@ function elk_conflicts_solving() {
     "securebrowsing/$CONTAINER_TAG" elkConflicts
 }
 
+
+function get_latest_version() {
+    cd "$ES_PATH"
+
+    source "$ES_PATH"/ericomshield-repo.sh
+
+    if [ -z "$VERSION_FILE_PATH" ]; then
+        VERSION_FILE_PATH="$ES_repo_ver"
+    fi
+
+    echo "$VERSION_FILE_PATH"
+    curl -s -S -o "$ES_VER_FILE_NEW" "$VERSION_FILE_PATH"
+    if [ ! -f "$ES_VER_FILE_NEW" ] || [ $(grep -c "$NOT_FOUND_STR" "$ES_VER_FILE_NEW") -ge 1 ]; then
+        echo "Download version file failed. Please check shield version $BRANCH is correct"
+        exit 1
+    fi
+    mv "$ES_VER_FILE_NEW" "$ES_VER_FILE"
+    echo $BRANCH >"$ES_BRANCH_FILE"
+
+    if [ -n "$KEY_INSTALL" ]; then
+        BRANCH=""
+        ES_VERSION_ARG=""
+    fi
+}
+
 while [ $# -ne 0 ]; do
     arg="$1"
     case "$arg" in
@@ -169,32 +194,6 @@ while [ $# -ne 0 ]; do
     esac
     shift
 done
-
-function get_latest_version() {
-    cd "$ES_PATH"
-
-    source "$ES_PATH"/ericomshield-repo.sh
-
-    if [ -z "$VERSION_FILE_PATH" ]; then
-        VERSION_FILE_PATH="$ES_repo_ver"
-    fi
-
-    echo "$VERSION_FILE_PATH"
-    curl -s -S -o "$ES_VER_FILE_NEW" "$VERSION_FILE_PATH"
-    if [ ! -f "$ES_VER_FILE_NEW" ] || [ $(grep -c "$NOT_FOUND_STR" "$ES_VER_FILE_NEW") -ge 1 ]; then
-        echo "Download version file failed. Please check shield version $BRANCH is correct"
-        exit 1
-    fi
-    mv "$ES_VER_FILE_NEW" "$ES_VER_FILE"
-    echo $BRANCH >"$ES_BRANCH_FILE"
-
-    if [ -n "$KEY_INSTALL" ]; then
-        BRANCH=""
-        ES_VERSION_ARG=""
-    fi
-}
-
-
 
 
 function read_current_version() {
