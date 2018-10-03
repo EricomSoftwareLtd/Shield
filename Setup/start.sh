e3c30b9b97e953ae189c8cb09e34aa8692c20b40
Beny H <Beny.Haddad@ericom.com>
2018-10-03 17:31:25 +0300

    Update start.sh

--- Setup/start.sh
+++ Setup/start.sh
@@ -3,15 +3,26 @@
 #####   Ericom Shield Run              #####
 #######################################BH###
 
-ES_PATH=/usr/local/ericomshield
+function show_usage() {
+    echo "Start Ericom Shield"
+    echo "Usage: $0 "
+    exit
+}
 
 #Check if we are root
 if ((EUID != 0)); then
     #    sudo su
     echo " Please run it as Root"
-    echo "sudo" $0 $1 $2
+    echo "sudo $0 $@"
+    show_usage
     exit
 fi
+
+if [ ! -z $1 ]; then
+    show_usage
+fi
+
+ES_PATH=/usr/local/ericomshield
 cd $ES_PATH
 
 echo "Running deploy-shield.sh:"
