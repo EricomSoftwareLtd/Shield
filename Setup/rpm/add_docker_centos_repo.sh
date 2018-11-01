8d4c60efd8af077c022581c5d442cafef8ae7d5a
User <as.asaw@gmail.com>
2018-11-01 11:25:26 +0300

    Add Docker CentOS repo script

---
+++ Setup/rpm/add_docker_centos_repo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -ex
+
+sudo yum install -y yum-utils \
+    device-mapper-persistent-data \
+    lvm2
+
+sudo yum-config-manager --add-repo "https://download.docker.com/linux/centos/docker-ce.repo"
+sudo yum-config-manager --disable docker-ce-edge
+sudo yum-config-manager --disable docker-ce-test
+
+# sudo yum install docker-ce
