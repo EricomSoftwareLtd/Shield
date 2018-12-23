7e8b6ad0ff2e3afb71c06230a5ea3bf9097476d4
ozlevka-ericom <lev.ozeryansky@ericom.com>
2018-12-23 15:05:29 +0200

    Remove stupid things from script

--- Setup/rpm/_build_in_docker.sh
+++ Setup/rpm/_build_in_docker.sh
@@ -9,7 +9,6 @@ function build_in_docker() {
     fi
     local DOCKER_OPTS=(--rm -w="/home/user/work" -v "${BUILD_DIR}:/home/user/work" --name "9A00342C-A532-4091-A59C-8DC445DBF9F3" "$DOCKER_RPM_BUILDER_IMG_TAG")
     docker run "${DOCKER_OPTS[@]}" /bin/bash -c "chown -R user:user '/home/user/work'"
-    chmod 666 "$BUILD_DIR"
     docker run --user="user:user" "${DOCKER_OPTS[@]}" /bin/bash -c "rpmbuild --define='_topdir /home/user/work' --define '_buildfor_rel rhel' -ba '/home/user/work/SPECS/ericom_shield.spec'"
     docker run --user="user:user" "${DOCKER_OPTS[@]}" /bin/bash -c "rpmbuild --define='_topdir /home/user/work' --define '_buildfor_rel centos' -ba '/home/user/work/SPECS/ericom_shield.spec'"
     docker run "${DOCKER_OPTS[@]}" /bin/bash -c "chown -R $(id -u):$(id -g) /home/user/work"
