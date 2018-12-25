05e9244b9b4ed618d7ac48fe731c1989a53d0011
Beny H <Beny.Haddad@ericom.com>
2018-12-25 19:44:16 +0200

    Update _build_native.sh

--- Setup/rpm/_build_native.sh
+++ Setup/rpm/_build_native.sh
@@ -4,7 +4,10 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" #"
 source "${DIR}/common"
 
 function build_native() {
-    rpmbuild --define="_topdir ${BUILD_DIR}" -ba "${BUILD_DIR}/SPECS/ericom_shield.spec"
+    rpmbuild --define="_topdir ${BUILD_DIR}" --define '_buildfor_rel rhel' -ba "${BUILD_DIR}/SPECS/ericom_shield.spec"
+    rpmbuild --define="_topdir ${BUILD_DIR}" --define '_buildfor_rel centos' -ba "${BUILD_DIR}/SPECS/ericom_shield.spec"
+    cp "${BUILD_DIR}/RPMS/x86_64/ericom_shield-"*".rhel.x86_64.rpm" "${BUILD_DIR}/RPMS/x86_64/ericom_shield.rhel.x86_64.rpm"
+    cp "${BUILD_DIR}/RPMS/x86_64/ericom_shield-"*".centos.x86_64.rpm" "${BUILD_DIR}/RPMS/x86_64/ericom_shield.centos.x86_64.rpm"
 }
 
 extract_versions "${DIR}/../shield-version.txt"
