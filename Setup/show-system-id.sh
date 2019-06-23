639dd3cb7f61e6501f9f55879734967bf4ea72d9
Beny <beny.haddad@ericom.com>
2019-06-23 06:16:49 -0400

    shield-pre-install

--- Setup/show-system-id.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-############################################
-#####   Ericom Shield Show System ID   #####
-#######################################BH###
-
-echo "Ericom Shield System ID:"
-docker container exec $(docker ps --filter name=broker -q) cat /run/secrets/shield-system-id
