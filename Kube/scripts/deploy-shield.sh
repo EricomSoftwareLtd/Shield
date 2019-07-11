fc5a517f5b641a37fbab0d6fa05c79f00ccfcde4
Andrew Novikov <as.asaw@gmail.com>
2019-07-11 18:19:49 +0300

    Format deploy-shield.sh

--- Kube/scripts/deploy-shield.sh
+++ Kube/scripts/deploy-shield.sh
@@ -29,8 +29,8 @@ VERSION_DEPLOYED=$(helm list shield | grep shield | awk '{ print $9 }')
 echo "Current Version: $VERSION_DEPLOYED"
 
 if [ "$VERSION_REPO" == "$VERSION_DEPLOYED" ]; then
-   echo "Same Versions, nothing to do"
-   exit
+    echo "Same Versions, nothing to do"
+    exit
 fi
 
 CUSTOM_VALUES_FILE="custom-values.yaml"
@@ -39,8 +39,8 @@ if [ -n "$1" ]; then
     CUSTOM_VALUES_FILE="$1"
 
     if [ ! -f "$CUSTOM_VALUES_FILE" ]; then
-       echo "File $CUSTOM_VALUES_FILE not found"
-       exit 1
+        echo "File $CUSTOM_VALUES_FILE not found"
+        exit 1
     fi
 fi
 
@@ -52,43 +52,43 @@ fi
 log_message "***************     Deploying Ericom Shield $VERSION_REPO ..."
 
 if [ "$SHIELD_FARM" == "yes" ]; then
-   log_message "***************     Deploying Shield Farm Services *******************************"
-   if [ "$SET_LABELS" == "yes" ]; then   
-      kubectl label node --all shield-role/farm-services=accept --overwrite
-      kubectl label node --all shield-role/remote-browsers=accept --overwrite
-   fi   
-   curl -s -o custom-farm.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-farm.yaml      
-   helm upgrade --install shield-farm-services shield-repo/shield --namespace=farm-services -f custom-farm.yaml --debug | tee -a "$LOGFILE"
-   sleep 30
-fi   
+    log_message "***************     Deploying Shield Farm Services *******************************"
+    if [ "$SET_LABELS" == "yes" ]; then
+        kubectl label node --all shield-role/farm-services=accept --overwrite
+        kubectl label node --all shield-role/remote-browsers=accept --overwrite
+    fi
+    curl -s -o custom-farm.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-farm.yaml
+    helm upgrade --install shield-farm-services shield-repo/shield --namespace=farm-services -f custom-farm.yaml --debug | tee -a "$LOGFILE"
+    sleep 30
+fi
 
 if [ "$SHIELD_MNG" == "yes" ]; then
-   log_message "***************     Deploying Shield Management *******************************"
-   if [ "$SET_LABELS" == "yes" ]; then      
-     kubectl label node --all shield-role/management=accept --overwrite
-   fi  
-   curl -s -o custom-management.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-management.yaml
-   helm upgrade --install shield-management    shield-repo/shield --namespace=management -f custom-management.yaml --debug | tee -a "$LOGFILE"
-   sleep 30
+    log_message "***************     Deploying Shield Management *******************************"
+    if [ "$SET_LABELS" == "yes" ]; then
+        kubectl label node --all shield-role/management=accept --overwrite
+    fi
+    curl -s -o custom-management.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-management.yaml
+    helm upgrade --install shield-management shield-repo/shield --namespace=management -f custom-management.yaml --debug | tee -a "$LOGFILE"
+    sleep 30
 fi
 
 if [ "$SHIELD_PROXY" == "yes" ]; then
-   log_message "***************     Deploying Shield Proxy *******************************"
-   if [ "$SET_LABELS" == "yes" ]; then      
-     kubectl label node --all shield-role/proxy=accept --overwrite
-   fi  
-   curl -s -o custom-proxy.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-proxy.yaml   
-   helm upgrade --install shield-proxy         shield-repo/shield --namespace=proxy --set-string "shield-proxy.UPSTREAM_DNS_SERVERS=${UPSTREAM_DNS_SERVERS}" -f custom-proxy.yaml --debug | tee -a "$LOGFILE"
-   sleep 30
-fi   
+    log_message "***************     Deploying Shield Proxy *******************************"
+    if [ "$SET_LABELS" == "yes" ]; then
+        kubectl label node --all shield-role/proxy=accept --overwrite
+    fi
+    curl -s -o custom-proxy.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-proxy.yaml
+    helm upgrade --install shield-proxy shield-repo/shield --namespace=proxy --set-string "shield-proxy.UPSTREAM_DNS_SERVERS=${UPSTREAM_DNS_SERVERS}" -f custom-proxy.yaml --debug | tee -a "$LOGFILE"
+    sleep 30
+fi
 
 if [ "$SHIELD_ELK" == "yes" ]; then
-   log_message "***************     Deploying Shield ELK *******************************"
-   if [ "$SET_LABELS" == "yes" ]; then      
-      kubectl label node --all shield-role/elk=accept --overwrite
-   fi   
-   curl -s -o custom-values-elk.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-values-elk.yaml   
-   helm upgrade --install shield-elk           shield-repo/shield --namespace=elk -f custom-values-elk.yaml --debug | tee -a "$LOGFILE"
-fi   
+    log_message "***************     Deploying Shield ELK *******************************"
+    if [ "$SET_LABELS" == "yes" ]; then
+        kubectl label node --all shield-role/elk=accept --overwrite
+    fi
+    curl -s -o custom-values-elk.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-values-elk.yaml
+    helm upgrade --install shield-elk shield-repo/shield --namespace=elk -f custom-values-elk.yaml --debug | tee -a "$LOGFILE"
+fi
 
 log_message "***************     Done!"
