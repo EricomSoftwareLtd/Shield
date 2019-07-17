b2c0bb39b823a57b12ffbd06e1d758809b25f501
Andrew Novikov <as.asaw@gmail.com>
2019-07-17 18:21:59 +0300

    Support CentOS

--- Kube/scripts/install-docker.sh
+++ Kube/scripts/install-docker.sh
@@ -11,7 +11,6 @@ function usage() {
     echo " Usage: $0 "
 }
 
-
 #Check if we are root
 if ((EUID != 0)); then
     #    sudo su
@@ -36,11 +35,12 @@ while [ $# -ne 0 ]; do
     shift
 done
 
-if [ ! -x $APP_BIN ]  || [ "$($APP_BIN version | grep -c $APP_VERSION)" -le 1 ] || [ $ES_FORCE == true ]; then
-   echo "Installing $APP ..."
-   curl -fsSL https://get.docker.com -o get-docker.sh
-   sh get-docker.sh
-   echo "Done!"
- else
-  echo "$APP is already installed"
+if [ ! -x $APP_BIN ] || [ "$($APP_BIN version | grep -c $APP_VERSION)" -le 1 ] || [ $ES_FORCE == true ]; then
+    echo "Installing $APP ..."
+    curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+    sh /tmp/get-docker.sh
+    rm -f /tmp/get-docker.sh
+    echo "Done!"
+else
+    echo "$APP is already installed"
 fi
