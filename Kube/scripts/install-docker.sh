a38a8348b64b6b3d15336fe0e6151a9463001100
Andrew Novikov <as.asaw@gmail.com>
2019-07-18 14:57:52 +0300

    Do not run configuration scripts as root unnecessarily

--- Kube/scripts/install-docker.sh
+++ Kube/scripts/install-docker.sh
@@ -11,15 +11,6 @@ function usage() {
     echo " Usage: $0 "
 }
 
-#Check if we are root
-if ((EUID != 0)); then
-    #    sudo su
-    usage
-    echo " Please run it as Root"
-    echo "sudo $0 $@"
-    exit
-fi
-
 while [ $# -ne 0 ]; do
     arg="$1"
     case "$arg" in
@@ -38,7 +29,7 @@ done
 if [ ! -x $APP_BIN ] || [ "$($APP_BIN version | grep -c $APP_VERSION)" -le 1 ] || [ $ES_FORCE == true ]; then
     echo "Installing $APP ..."
     curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
-    sh /tmp/get-docker.sh
+    sudo sh /tmp/get-docker.sh
     rm -f /tmp/get-docker.sh
     echo "Done!"
 else
