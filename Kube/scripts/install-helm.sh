084880ca0f7dee79871e98f28b7feecb9ece8dd5
benyh <beny.haddad@ericom.com>
2019-08-07 06:23:42 -0400

    Rel-19.07.1

--- Kube/scripts/install-helm.sh
+++ Kube/scripts/install-helm.sh
@@ -7,7 +7,7 @@ APP_VERSION="v2.14.2"
 APP_BIN="helm"
 ES_FORCE=false
 ES_CLEAN=false
-BRANCH="Staging"
+BRANCH="Rel-19.07.1"
 
 function usage() {
     echo " Usage: $0 [-f | --force] [-c | --clean]"
