7a04f48d5a80d24313244a24bf54f9df09905bf8
Andrew Novikov <Andrew.Novikov@artezio.com>
2019-09-06 18:13:14 +0300

    Improve AppImage creation, pack proxy.py

--- Utils/proxy/_create_proxy_AppImage.sh
+++ Utils/proxy/_create_proxy_AppImage.sh
@@ -11,16 +11,35 @@ APPIMAGE="proxy-x86_64.AppImage"
 APPIMAGETOOL_URL="https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage"
 APPIMAGETOOL="/tmp/appimagetool-x86_64.AppImage"
 
-wget -O "$APPIMAGETOOL" -nc "$APPIMAGETOOL_URL"
-chmod +x "$APPIMAGETOOL"
+if ! [ -x "$APPIMAGETOOL" ]; then
+    wget -O "$APPIMAGETOOL" -nc "$APPIMAGETOOL_URL"
+    chmod +x "$APPIMAGETOOL"
+fi
 
 APPIMAGE_DIR="$(mktemp -d)" || {
     echo "Failed to create AppDir"
     exit 1
 }
 
+CONDAPKGS_DIR="$(mktemp -d)" || {
+    echo "Failed to create a temporary directory for Conda"
+    exit 1
+}
+
 cp "$DIR/AppRun" "$DIR/EricomShield.desktop" "$DIR/EricomShield.svg" "$APPIMAGE_DIR"
-docker run -v "$APPIMAGE_DIR:/AppDir" --rm continuumio/miniconda3 conda create -p AppDir/usr python --copy --no-default-packages -y
+docker run --user "$(id -u):$(id -g)" \
+    -v "$APPIMAGE_DIR:/AppDir" \
+    -v "$APPIMAGE_DIR:/.conda" \
+    -v "$CONDAPKGS_DIR:/Packages" \
+    --rm \
+    -e "CONDA_PKGS_DIRS=/Packages" \
+    -e "CONDA_ENVS_PATH=/AppDir" \
+    -w "/tmp" \
+    continuumio/miniconda3 conda create -p /AppDir/usr python --copy --no-default-packages -y
+
+cp -T "$DIR/../proxy3.py" "$APPIMAGE_DIR/usr/bin/proxy.py"
+
 "$APPIMAGETOOL" "$APPIMAGE_DIR" "$APPIMAGE"
 
-#rm -rf "$APPIMAGE_DIR"
+rm -rf "$APPIMAGE_DIR"
+rm -rf "$CONDAPKGS_DIR"
