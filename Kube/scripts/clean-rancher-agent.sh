e918d292f38c7c31be6ee2ab7ccc1a8ad90f1aed
takuyaarita <tarita@ashisuto.co.jp>
2019-09-26 11:13:05 +0900

    delete flannel in the clean-rancher-agent.sh

--- Kube/scripts/clean-rancher-agent.sh
+++ Kube/scripts/clean-rancher-agent.sh
@@ -24,7 +24,7 @@ sleep 5
 
 docker rm -f $(docker ps -qa)
 docker volume rm $(docker volume ls -q)
-cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico /opt/rke"
+cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico /var/run/flannel /opt/rke"
 for dir in $cleanupdirs; do
     echo "Removing $dir"
     rm -rf $dir
