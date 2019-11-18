7e1fb5a7cc39ac9d882aa7399e2344a835af21c6
BenyH <Beny.Haddad@ericom.com>
2019-11-18 03:23:33 -0500

    Dev587

--- Kube/scripts/add-shield-repo.sh
+++ Kube/scripts/add-shield-repo.sh
@@ -3,14 +3,83 @@
 #####   Ericom Shield Installer:add_repo   #####
 ###########################################BH###
 
-function usage() {
-    echo " Usage: $0 <-d|--dev> <-s|--staging> -p <PASSWORD> <-v|--version VERSION>"
-}
-
 SHIELD_REPO_URL="https://helmrepo.shield-service.net"
-SHIELD_REPO="$SHIELD_REPO_URL/dev"
+SHIELD_REPO="$SHIELD_REPO_URL/master"
+NOT_FOUND_STR="404: Not Found"
 
 PASSWORD=""
+ES_BRANCH_FILE=".esbranch"
+LOGFILE=last_deploy.log
+
+function usage() {
+    echo " Usage: $0 -p <PASSWORD> [-d|--dev] [-s|--staging] [-v|--version] <version-name> [--list-versions] "
+}
+
+function log_message() {
+    local PREV_RET_CODE=$?
+    echo "$@"
+    echo "$(LC_ALL=C date): $@" >>"$LOGFILE"
+    if ((PREV_RET_CODE != 0)); then
+        return 1
+    fi
+    return 0
+}
+
+function download_and_check(){
+    curl -s -S -o "$1" "$2"   
+    if [ ! -f "$1" ] || [ $(grep -c "$NOT_FOUND_STR" "$1") -ge 1 ]; then
+        echo "Error: cannot download "$1", exiting"
+        exit 1
+    fi
+}
+
+function list_versions() {
+    ES_repo_versions="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/Dev/Setup/Releases-kube.txt"
+    echo "Getting $ES_repo_versions"
+    download_and_check "Releases.txt" $ES_repo_versions
+
+    while true; do
+        cat Releases.txt | cut -d':' -f1
+        read -p "Please select the Release you want to install/update (1-4):" choice
+        case "$choice" in
+        "1" | "latest")
+            echo 'latest'
+            OPTION="1)"
+            break
+            ;;
+        "2")
+            echo "2."
+            OPTION="2)"
+            break
+            ;;
+        "3")
+            echo "3."
+            OPTION="3)"
+            break
+            ;;
+        "4")
+            echo "4."
+            OPTION="4)"
+            break
+            ;;
+        "5")
+            echo "5."
+            OPTION="5)"
+            break
+            ;;
+        *)
+            echo "Error: Not a valid option, exiting"
+            ;;
+        esac
+    done
+    grep "$OPTION" Releases.txt
+    BRANCH=$(grep "$OPTION" Releases.txt | cut -d':' -f3)
+    echo -n $BRANCH >"$ES_BRANCH_FILE"
+    REPO=$(grep "$OPTION" Releases.txt | cut -d':' -f2)
+    SHIELD_REPO="$SHIELD_REPO_URL/$REPO"
+    
+    echo "$SHIELD_REPO" "$BRANCH" 
+}
 
 while [ $# -ne 0 ]; do
     arg="$1"
@@ -23,11 +92,16 @@ while [ $# -ne 0 ]; do
         shift
         SHIELD_REPO="$SHIELD_REPO_URL/$1"
         ;;
-    -d | --dev | --Dev) # Dev Channel (master branch for now)
+    -d | --dev) # Dev Channel (dev branch)
         SHIELD_REPO="$SHIELD_REPO_URL/dev"
+        echo -n "Dev" >"$ES_BRANCH_FILE"
         ;;
-    -s | --staging | --Staging) # Dev Channel (Staging Branch)
+    -s | --staging) # Staging Channel (staging Branch)
         SHIELD_REPO="$SHIELD_REPO_URL/staging"
+        echo -n "Staging" >"$ES_BRANCH_FILE"        
+        ;;
+    -l | --list-versions) 
+        list_versions
         ;;
     *)
         usage "$0"
--- Kube/scripts/deploy-shield.sh
+++ Kube/scripts/deploy-shield.sh
@@ -7,10 +7,21 @@ SHIELD_PROXY="yes"
 SHIELD_FARM="yes"
 SHIELD_ELK="yes"
 SET_LABELS="No"
+ES_FORCE=false
+ES_OVERWRITE=false
+NOT_FOUND_STR="404: Not Found"
+ES_repo_EULA="https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/master/Setup/Ericom-EULA.txt"
+EULA_ACCEPTED_FILE=".eula_accepted"
+ES_BRANCH_FILE=".esbranch"
 BRANCH="Dev"
+SHIELD_NS_COUNT=5
 SHIELD_REPO="shield-repo"
 # For Local Use ..
 #SHIELD_REPO=".."
+SYSTEMID=$(kubectl get namespace kube-system -o=jsonpath='{.metadata.uid}')
+DRY_RUN="" #"--dry-run"
+
+echo $SYSTEMID
 
 # shield-role/management=accept
 # shield-role/proxy=accept
@@ -19,6 +30,10 @@ SHIELD_REPO="shield-repo"
 # shield-role/remote-browsers=accept
 
 LOGFILE=last_deploy.log
+
+function usage() {
+    echo " Usage: $0 [-n|--namespace <NAMESPACE>] [-l|--label] [-f|--force] [--help]"
+}
 
 function log_message() {
     local PREV_RET_CODE=$?
@@ -30,36 +45,155 @@ function log_message() {
     return 0
 }
 
-helm repo update
-helm search shield
+function download_and_check(){
+     curl -s -S -o "$1" "$2"   
+    if [ ! -f "$1" ] || [ $(grep -c "$NOT_FOUND_STR" "$1") -ge 1 ]; then
+        echo "Error: cannot download "$1", exiting"
+        exit 1
+    fi
+}
 
-log_message " *** Deploying Shield *** "
+function accept_license() {
+    export LESSSECURE=1
+    while less -P"%pb\% Press h for help or q to quit" "$1" &&
+        read -p "Do you accept the EULA (yes/no/anything else to display it again)? " choice; do
+        case "$choice" in
+        y | Y | n | N)
+            echo 'Please, type "yes" or "no"'
+            read -n1 -r -p "Press any key to continue..." key
+            ;;
+        "yes" | "YES" | "Yes")
+            echo "yes"
+            return 0
+            ;;
+        "no" | "NO" | "No")
+            echo "no"
+            break
+            ;;
+        *) ;;
 
-VERSION_REPO=$(helm search shield | grep shield | awk '{ print $2 }')
-log_message "Latest Version : $VERSION_REPO"
+        esac
+    done
 
-VERSION_DEPLOYED=$(helm list shield | grep -m 1 shield | awk '{ print $9 }')
-log_message "Current Version: $VERSION_DEPLOYED"
+    return -1
+}
 
-if [ "shield-$VERSION_REPO" == "$VERSION_DEPLOYED" ]; then
-    echo "Same Versions, nothing to do"
-    exit
-fi
+function accept_eula() {
+    download_and_check "Ericom-EULA.txt" "$ES_repo_EULA"
+    if [ ! -f "$EULA_ACCEPTED_FILE" ]; then
+       echo 'You will now be presented with the End User License Agreement.'
+       echo 'Use PgUp/PgDn/Arrow keys for navigation, q to exit.'
+       echo 'Please, read the EULA carefully, then accept it to continue the installation process or reject to exit.'
+       read -n1 -r -p "Press any key to continue..." key
+       echo
+
+       if accept_license "Ericom-EULA.txt"; then
+          log_message "EULA has been accepted"
+          date -Iminutes >"$EULA_ACCEPTED_FILE"
+       else
+          log_message "EULA has not been accepted, exiting..."
+          exit
+       fi
+    fi
+}
 
 get_timezone() {
     local TZ
     if [ -h /etc/localtime ]; then
-        TZ=":$(readlink /etc/localtime | sed 's|.*/usr/share/zoneinfo/||')"
+        TZ=":$(readlink /etc/localtime | sed 's/\/usr\/share\/zoneinfo\///')"
     elif [ -f /etc/timezone ]; then
         TZ="$(cat /etc/timezone)"
     elif [ -f /etc/localtime ]; then
-        TZ=":$(find /usr/share/zoneinfo/ -type f -exec md5sum {} \; | grep "^$(md5sum /etc/localtime | cut -d' ' -f1)" | sed 's|.*/usr/share/zoneinfo/||' | head -n 1)" #"
+        TZ=":$(find /usr/share/zoneinfo/ -type f -exec md5sum {} \; | grep "^$(md5sum /etc/localtime | cut -d' ' -f1)" | sed 's/.*\/usr\/share\/zoneinfo\///' | head -n 1)" #"
     else
         TZ="$(date +%Z)"
     fi
 
     echo $TZ
 }
+
+only_namespace(){
+   SHIELD_MNG="no"
+   SHIELD_PROXY="no"
+   SHIELD_FARM="no"
+   SHIELD_ELK="no"
+   case "$1" in
+    shield-management)
+        SHIELD_MNG="yes"
+     ;;    
+    shield-proxy)
+        SHIELD_PROXY="yes"
+     ;;    
+    shield-farm)
+        SHIELD_FARM="yes"
+     ;;    
+    shield-elk)     
+        SHIELD_ELK="yes"
+     ;;
+    *)
+        usage
+        exit
+        ;;
+   esac 
+}
+
+while [ $# -ne 0 ]; do
+    arg="$1"
+    case "$arg" in
+    -n | --namespace) 
+        shift
+        only_namespace "$1"
+        ;;
+    -l | --label) 
+        SET_LABELS="yes"
+        ;;
+    -o | --overwrite)
+        ES_OVERWRITE=true
+        ;;
+    -f | --force)
+        ES_FORCE=true
+        ;;
+    -approve-eula)
+        log_message "EULA has been accepted from Command Line"
+        date -Iminutes >"$EULA_ACCEPTED_FILE"
+        ;;
+    #        -help)
+    *)
+        usage
+        exit
+        ;;
+    esac
+    shift
+done
+
+
+##################      MAIN: EVERYTHING STARTS HERE: ##########################
+
+log_message "***************     Ericom Shield Kube Setup $BRANCH ..."
+
+accept_eula
+
+helm repo update
+helm search shield
+
+VERSION_REPO=$(helm search shield | grep shield | awk '{ print $2 }')
+log_message "Latest Version on Repo: $VERSION_REPO"
+
+VERSION_DEPLOYED=$(helm list | grep -m 1 shield | awk '{ print $9 }')
+if [ ! -z $VERSION_DEPLOYED ] && [ $(helm list | grep -c $VERSION_DEPLOYED) == "$SHIELD_NS_COUNT" ]; then
+   VERSION_DEPLOYED=$(helm list | grep -m 1 shield | awk '{ print $9 }')
+   log_message "Current Version Deployed: $VERSION_DEPLOYED"
+  else
+   log_message "Shield is not fully deployed"    
+   VERSION_DEPLOYED=""
+fi
+
+if [ "shield-$VERSION_REPO" == "$VERSION_DEPLOYED" ]; then
+    echo "Your EricomShield System is Up to date ($VERSION_REPO)"
+    if [ ! $ES_FORCE = true ]; then
+       exit
+    fi   
+fi
 
 TZ="$(get_timezone)"
 
@@ -70,17 +204,25 @@ fi
 
 log_message "***************     Deploying Ericom Shield $VERSION_REPO ..."
 
+if [ -f "$ES_BRANCH_FILE" ]; then
+    BRANCH=$(cat "$ES_BRANCH_FILE")
+ else
+    BRANCH="master"
+fi
+
 if [ "$SHIELD_FARM" == "yes" ]; then
     log_message "***************     Deploying Shield Farm Services *******************************"
     if [ "$SET_LABELS" == "yes" ]; then
         kubectl label node --all shield-role/farm-services=accept --overwrite
         kubectl label node --all shield-role/remote-browsers=accept --overwrite
     fi
-    if [ ! -f "custom-farm.yaml" ]; then
-        curl -s -o custom-farm.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-farm.yaml
+    if [ $ES_OVERWRITE ] || [ ! -f "custom-farm.yaml" ]; then
+        download_and_check custom-farm.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-farm.yaml
     fi
 
-    helm upgrade --install shield-farm-services $SHIELD_REPO/shield --namespace=farm-services --set-string "farm-services.TZ=${TZ}" -f custom-farm.yaml --debug | tee -a "$LOGFILE"
+    helm upgrade --install shield-farm-services $SHIELD_REPO/shield --namespace=farm-services\
+                 --set-string "farm-services.TZ=${TZ}" --set-string "farm-services.CLUSTER_SYSTEM_ID=$SYSTEMID"\
+                 -f custom-farm.yaml --debug | tee -a "$LOGFILE"
     sleep 30
 fi
 
@@ -89,10 +231,13 @@ if [ "$SHIELD_MNG" == "yes" ]; then
     if [ "$SET_LABELS" == "yes" ]; then
         kubectl label node --all shield-role/management=accept --overwrite
     fi
-    if [ ! -f "custom-management.yaml" ]; then
-        curl -s -o custom-management.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-management.yaml
+    if [ $ES_OVERWRITE ] || [ ! -f "custom-management.yaml" ]; then
+        download_and_check custom-management.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-management.yaml
     fi
-    helm upgrade --install shield-management $SHIELD_REPO/shield --namespace=management --set-string "shield-mng.TZ=${TZ}" -f custom-management.yaml --debug | tee -a "$LOGFILE"
+    helm upgrade --install shield-management $SHIELD_REPO/shield --namespace=management\
+                 --set-string "management.TZ=${TZ}" --set-string "management.CLUSTER_SYSTEM_ID=$SYSTEMID"\
+                 -f custom-management.yaml --debug | tee -a "$LOGFILE"
+
     sleep 30
 fi
 
@@ -101,10 +246,13 @@ if [ "$SHIELD_PROXY" == "yes" ]; then
     if [ "$SET_LABELS" == "yes" ]; then
         kubectl label node --all shield-role/proxy=accept --overwrite
     fi
-    if [ ! -f "custom-proxy.yaml" ]; then
-        curl -s -o custom-proxy.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-proxy.yaml
+    if [ ES_OVERWRITE ] || [ ! -f "custom-proxy.yaml" ]; then
+        download_and_check custom-proxy.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-proxy.yaml
     fi
-    helm upgrade --install shield-proxy $SHIELD_REPO/shield --namespace=proxy --set-string "shield-proxy.TZ=${TZ}" --set-string "shield-proxy.UPSTREAM_DNS_SERVERS=$(echo ${UPSTREAM_DNS_SERVERS} | sed 's#,#\\,#g')" -f custom-proxy.yaml --debug | tee -a "$LOGFILE"
+    helm upgrade --install shield-proxy $SHIELD_REPO/shield --namespace=proxy\
+                 --set-string "proxy.TZ=${TZ}" --set-string "proxy.CLUSTER_SYSTEM_ID=$SYSTEMID"\
+                 -f custom-proxy.yaml --debug | tee -a "$LOGFILE"
+
     sleep 30
 fi
 
@@ -113,15 +261,18 @@ if [ "$SHIELD_ELK" == "yes" ]; then
     if [ "$SET_LABELS" == "yes" ]; then
         kubectl label node --all shield-role/elk=accept --overwrite
     fi
-    if [ ! -f "custom-values-elk" ]; then
-        curl -s -o custom-values-elk.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-values-elk.yaml
+    if [ $ES_OVERWRITE ] || [ ! -f "custom-values-elk" ]; then
+        download_and_check custom-values-elk.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-values-elk.yaml
     fi
-    helm upgrade --install shield-elk $SHIELD_REPO/shield --namespace=elk -f custom-values-elk.yaml --debug | tee -a "$LOGFILE"
+    helm upgrade --install shield-elk $SHIELD_REPO/shield --namespace=elk\
+                 --set-string "elk.TZ=${TZ}" --set-string "elk.CLUSTER_SYSTEM_ID=$SYSTEMID"\
+                 -f custom-values-elk.yaml --debug | tee -a "$LOGFILE"
+
 fi
 
 log_message "***************     Deploying Shield Common *******************************"
-if [ ! -f "custom-common.yaml" ]; then
-    curl -s -o custom-common.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-common.yaml
+if [ $ES_OVERWRITE ] || [ ! -f "custom-common.yaml" ]; then
+    download_and_check custom-common.yaml https://raw.githubusercontent.com/EricomSoftwareLtd/Shield/$BRANCH/Kube/scripts/custom-common.yaml
 fi
 helm upgrade --install shield-common $SHIELD_REPO/shield --namespace=common -f custom-common.yaml --debug | tee -a "$LOGFILE"
 
