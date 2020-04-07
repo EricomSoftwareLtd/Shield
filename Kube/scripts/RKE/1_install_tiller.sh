efb8cc7776748d30385683208453368f1447ead7
Andrew Novikov <as.asaw@gmail.com>
2020-04-07 16:11:30 +0300

    Update 1_install_tiller.sh to work with helm 2.16.5

--- Kube/scripts/RKE/1_install_tiller.sh
+++ Kube/scripts/RKE/1_install_tiller.sh
@@ -8,7 +8,7 @@ kubectl create clusterrolebinding tiller \
     --clusterrole=cluster-admin \
     --serviceaccount=kube-system:tiller
 
-helm init --service-account tiller
+helm init --wait --service-account tiller
 
 # Users in China: You will need to specify a specific tiller-image in order to initialize tiller.
 # The list of tiller image tags are available here: https://dev.aliyun.com/detail.html?spm=5176.1972343.2.18.ErFNgC&repoId=62085.
@@ -18,4 +18,3 @@ helm init --service-account tiller
 #  --tiller-image registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:<tag>
 
 kubectl -n kube-system rollout status deploy/tiller-deploy
-kubectl --namespace=kube-system wait --for=condition=Available --timeout=5m apiservices/v1beta1.metrics.k8s.io
