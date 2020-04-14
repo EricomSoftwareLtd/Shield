33d0f3e5c71f7e75837bc7534af95adfacec4aec
Andrew Novikov <as.asaw@gmail.com>
2020-04-14 18:05:57 +0300

    Make add-registry.py idempotent (fix repetative calls)

--- Kube/PrepareNode/add-registry.py
+++ Kube/PrepareNode/add-registry.py
@@ -3,31 +3,49 @@
 import yaml
 import sys
 import argparse
+import re
+
+ADDR_RE_PATTERN = re.compile(r"^([.:\-a-zA-Z0-9]+)[/]?.*")
+
+
+def image_has_reg(reg, str):
+    match = ADDR_RE_PATTERN.search(str)
+    if match:
+        if match.group(1) == reg:
+            print('"{0}" already contains "{1}", skipping...'.format(str, reg))
+            return True
+    return False
 
 
 def add_in_subsection(reg, subsection):
     for i in subsection:
+        if image_has_reg(reg, subsection[i]):
+            continue
         subsection[i] = "{0}/{1}".format(reg, subsection[i])
 
 
 def add_in_subsection_elk(reg, subsection):
+    if image_has_reg(reg, subsection['image']):
+        return
     subsection['image'] = "{0}/{1}".format(reg, subsection['image'])
 
 
 def add_in_subsection_consul(reg, subsection):
+    if image_has_reg(reg, subsection['registry']):
+        return
     subsection['repository'] = "{0}/{1}".format(
         subsection['registry'], subsection['repository'])
     subsection['registry'] = reg
 
 
 parser = argparse.ArgumentParser(
-    description='Add Offline Registry address to custom values YAML.')
+    description='Add Offline Registry address to Shield Helm chart YAML.')
 parser.add_argument('repo', type=str,
                     nargs=1, help='IP address and port of the Offline Registry <address:port>')
 parser.add_argument('shield_yaml_name', type=str,
-                    nargs=1, help='Shield custom values YAML file')
+                    nargs=1, help='Shield chart YAML file')
 parser.add_argument('consul_yaml_name', type=str,
-                    nargs='?', help='Consul custom values YAML file')
+                    nargs='?', help='Consul chart YAML file')
 
 args = parser.parse_args()
 
