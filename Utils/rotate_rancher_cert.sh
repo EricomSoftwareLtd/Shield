bde868ef53d7178a53d05d9700ea632631146ad4
Beny H <Beny.Haddad@ericom.com>
2021-03-24 15:01:17 +0200

    Suggested changes from KKA

--- Utils/rotate_rancher_cert.sh
+++ Utils/rotate_rancher_cert.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -ex
 
 export RANCHER_CONTAINER_ID=$(docker ps | grep -oP '\w+(?=\s+rancher/rancher)') #'
+docker container restart $RANCHER_CONTAINER_ID
 
 docker exec -it $RANCHER_CONTAINER_ID sh -c 'mv /var/lib/rancher/k3s/server/tls /var/lib/rancher/k3s/server/tls.$(date -Iseconds)'
 docker container restart $RANCHER_CONTAINER_ID
