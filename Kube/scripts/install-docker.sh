97a5ac2bd986f57fc2bc935697d8d8d7213ac4a6
Beny H <Beny.Haddad@ericom.com>
2021-07-22 10:06:09 +0300

    Update install-docker.sh

--- Kube/scripts/install-docker.sh
+++ Kube/scripts/install-docker.sh
@@ -5,7 +5,7 @@
 APP="docker"
 APP_BIN="/usr/bin/docker"
 APP_VERSION="19.03"
-CONTAINERD_VERSION="1.2"
+DEB_APP_VERSION="5:$APP_VERSION"
 ES_FORCE=false
 
 function usage() {
@@ -19,7 +19,7 @@ while [ $# -ne 0 ]; do
         ES_FORCE=true
         ;;
     -h | --help)
-    #*)
+        #*)
         usage
         exit
         ;;
@@ -29,13 +29,19 @@ done
 
 if [ ! -x "$APP_BIN" ] || [ "$($APP_BIN version | grep -c $APP_VERSION)" -le 0 ] || [ "$ES_FORCE" = true ]; then
     echo "Installing $APP ..."
-    sudo apt-get update || exit $?
-    sudo apt-get -y install software-properties-common || exit $?
-    sudo add-apt-repository universe || exit $?
-    sudo apt-get update || exit $?
-    sudo apt-mark unhold docker.io
-    sudo apt-get install -y "docker.io=${APP_VERSION}*" "containerd=${CONTAINERD_VERSION}*" || exit $?
-    sudo apt-mark hold docker.io
+    if [ -z "$(apt-cache search --names-only '^docker-ce$')" ]; then
+        DOCKER_SCRIPT_URL="https://releases.rancher.com/install-docker/$APP_VERSION.sh"
+        curl -LJ --progress-bar $DOCKER_SCRIPT_URL | sudo sh
+    else
+        sudo apt-get update || exit $?
+        sudo apt-get -y install software-properties-common || exit $?
+        sudo add-apt-repository universe || exit $?
+        sudo apt-get update || exit $?
+        sudo apt-get purge docker.io containerd || exit $?
+        sudo apt-mark unhold docker-ce
+        sudo apt-get install --allow-downgrades -y "docker-ce=${DEB_APP_VERSION}*" "docker-ce-cli=${DEB_APP_VERSION}*" "containerd.io" || exit $?
+    fi
+    sudo apt-mark hold docker-ce
     sudo systemctl enable --now docker || exit $?
     sudo usermod -aG docker "$USER"
     echo "Done!"
