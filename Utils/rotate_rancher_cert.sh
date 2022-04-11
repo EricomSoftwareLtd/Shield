6a792d57e4f41492d67ad9cc0c0b9f41df84b422
Andrew Novikov <as.asaw@gmail.com>
2022-04-11 23:24:48 +0300

    Fix rotate_rancher_cert.sh to work correctly with Rancher 2.4.15

--- Utils/rotate_rancher_cert.sh
+++ Utils/rotate_rancher_cert.sh
@@ -1,9 +1,32 @@
-#!/bin/sh -ex
+#!/usr/bin/sudo /bin/bash
 
-export RANCHER_CONTAINER_ID=$(docker ps | grep -oP '\w+(?=\s+rancher/rancher:)') #'
-docker container restart $RANCHER_CONTAINER_ID
+set -e
+set -x
 
-docker exec -it $RANCHER_CONTAINER_ID sh -c 'mv /var/lib/rancher/k3s/server/tls /var/lib/rancher/k3s/server/tls.$(date -Iseconds)'
+export LC_TIME=C
+
+DATE_ORIG="$(date -d 'now' '+%F %T')"
+DATE_ORIG_ISO="$(date -Iseconds)"
+CERT_END_DATE="$(openssl x509 -noout -enddate -in "/home/ericom/ericomshield/rancher-store/k3s/server/tls/client-admin.crt" | sed -E 's/notAfter=(.*)/\1/')"
+DATE_PAST="$(date -d "$CERT_END_DATE - 1 months" '+%F %T')"
+
+function cleanup() {
+    timedatectl set-ntp off
+    timedatectl set-time "$DATE_ORIG"
+    timedatectl set-ntp on
+}
+
+trap cleanup EXIT
+
+RANCHER_CONTAINER_ID=$(docker ps | grep 'rancher/rancher:' | awk '{print $1}')
+
+docker exec -it $RANCHER_CONTAINER_ID sh -c "kubectl delete secret -n kube-system k3s-serving --insecure-skip-tls-verify" || :
+
+timedatectl set-ntp off
+timedatectl set-time "$DATE_PAST"
+
 docker container restart $RANCHER_CONTAINER_ID
 sleep 150
+cleanup
+docker exec -it $RANCHER_CONTAINER_ID sh -c "mv /var/lib/rancher/k3s/server/tls/dynamic-cert.json /var/lib/rancher/k3s/server/tls/dynamic-cert.json.${DATE_ORIG_ISO}" || :
 docker container restart $RANCHER_CONTAINER_ID
